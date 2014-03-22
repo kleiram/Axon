7690525dc47215f8d80ad5d63eef29214bfe29a8
Ramon Kleiss <ramonkleiss@gmail.com>
2014-03-22 20:49:24 +0100

    Defined the ProviderInterface

---
+++ lib/Axon/Provider/ProviderInterface.php
@@ -0,0 +1,22 @@
+<?php
+namespace Axon\Provider;
+
+use Axon\Model\Torrent;
+
+/**
+ * The interface torrent search providers must implement
+ *
+ * @author Ramon Kleiss <ramonkleiss@gmail.com>
+ */
+interface ProviderInterface
+{
+    /**
+     * Search the provider using a specified `query`
+     *
+     * @param string  $query The search query
+     * @param integer $page  (optional) The page to search on
+     *
+     * @return Torrent[]
+     */
+    public function search($query, $page = null);
+}
