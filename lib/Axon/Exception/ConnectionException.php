8d4793fabf3939d723984f5615a5b0d16217bb64
Ramon Kleiss <ramonkleiss@gmail.com>
2014-03-22 22:25:38 +0100

    Create the ConnectionException class

---
+++ lib/Axon/Exception/ConnectionException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Axon\Exception;
+
+/**
+ * A `ConnectionException` is thrown when a connection with an API
+ * could not be established
+ *
+ * @author Ramon Kleiss <ramonkleiss@gmail.com>
+ */
+class ConnectionException extends \RuntimeException
+{
+}
