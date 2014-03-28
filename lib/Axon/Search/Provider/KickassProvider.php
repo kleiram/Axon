a6cad023b87d549c220c47dbf07ee02fc73fe6bf
Ramon Kleiss <ramonkleiss@gmail.com>
2014-03-28 18:36:36 +0100

    Create the Kickass search provider

---
+++ lib/Axon/Search/Provider/KickassProvider.php
@@ -0,0 +1,129 @@
+<?php
+namespace Axon\Search\Provider;
+
+use Buzz\Browser;
+use Nomnom\Nomnom;
+use Symfony\Component\DomCrawler\Crawler;
+use Axon\Search\Model\Torrent;
+use Axon\Search\Exception\ConnectionException;
+use Axon\Search\Exception\UnexpectedResponseException;
+
+/**
+ * @author Ramon Kleiss <ramonkleiss@gmail.com>
+ */
+class KickassProvider implements ProviderInterface
+{
+    /**
+     * @var string
+     */
+    const DEFAULT_HOST = 'kickass.to';
+
+    /**
+     * @var string
+     */
+    const DEFAULT_PATH = '/usearch';
+
+    /**
+     * @var Browser
+     */
+    protected $browser;
+
+    /**
+     * @param Browser $browser
+     */
+    public function __construct(Browser $browser = null)
+    {
+        $this->browser = $browser ?: new Browser();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getName()
+    {
+        return 'kickass';
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function search($query, $page = null)
+    {
+        $url = $this->getUrl($query, $page);
+
+        try {
+            $response = $this->browser->get($url);
+        } catch (\Exception $e) {
+            throw new ConnectionException(sprintf(
+                'Could not connect to "%s"', $url
+            ), 0, $e);
+        }
+
+        if ($response->getStatusCode() != 200) {
+            throw new UnexpectedResponseException(sprintf(
+                'Unexpected response: %s (%d)',
+                $response->getReasonPhrase(),
+                $response->getStatusCode()
+            ));
+        }
+
+        return $this->transformResponse($response->getContent());
+    }
+
+    /**
+     * @param string       $query
+     * @param integer|null $page
+     *
+     * @return string
+     */
+    public function getUrl($query, $page)
+    {
+        if (is_integer($page)) {
+            return sprintf(
+                'http://%s%s/%s/%s/?field=seeders&order=desc',
+                self::DEFAULT_HOST,
+                self::DEFAULT_PATH,
+                $query,
+                $page
+            );
+        } else {
+            return sprintf(
+                'http://%s%s/%s/?field=seeders&order=desc',
+                self::DEFAULT_HOST,
+                self::DEFAULT_PATH,
+                $query
+            );
+        }
+    }
+
+    /**
+     * @param string $rawResponse
+     *
+     * @return Torrent[]
+     */
+    protected function transformResponse($rawResponse)
+    {
+        $crawler = new Crawler($rawResponse);
+
+        return $crawler->filter('tr[id^="torrent_"]')->each(function ($node) {
+            $magnet = $node->filter('a.imagnet')->attr('href');
+            preg_match('/btih:([0-9A-Za-z]+)&/', $magnet, $matches);
+            $hash = $matches[1];
+
+            $size = $node->filter('td.nobr')->text();
+            preg_match('/([0-9\.]+) ([A-Za-z]+)/', $size, $matches);
+            $size = $matches[1];
+            $unit = $matches[2];
+
+            $converter = new Nomnom($size);
+            $torrent = new Torrent();
+            $torrent->setName($node->filter('a.cellMainLink')->text());
+            $torrent->setHash($hash);
+            $torrent->setSize($converter->from($unit)->to('B'));
+            $torrent->setSeeds($node->filter('td.green')->text());
+            $torrent->setPeers($node->filter('td.red')->text());
+
+            return $torrent;
+        });
+    }
+}
---
+++ tests/Axon/Tests/Search/Provider/KickassProviderTest.php
@@ -0,0 +1,127 @@
+<?php
+namespace Axon\Tests\Search\Provider;
+
+use Axon\Search\Provider\KickassProvider;
+
+class KickassProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldImplementProviderInterface()
+    {
+        $provider = new KickassProvider();
+
+        $this->assertInstanceOf(
+            'Axon\Search\Provider\ProviderInterface',
+            $provider
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function shouldHaveName()
+    {
+        $provider = new KickassProvider();
+
+        $this->assertEquals('kickass', $provider->getName());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldSearch()
+    {
+        $response = $this->getMock('Buzz\Message\Response');
+        $response
+            ->expects($this->any())
+            ->method('getStatusCode')
+            ->will($this->returnValue(200));
+
+        $response
+            ->expects($this->once())
+            ->method('getContent')
+            ->will($this->returnValue(file_get_contents(__DIR__.'/../../../../fixtures/search/kat.html')));
+
+        $browser = $this->getMock('Buzz\Browser');
+        $browser
+            ->expects($this->once())
+            ->method('get')
+            ->will($this->returnValue($response));
+
+        $provider = new KickassProvider($browser);
+        $torrents = $provider->search('foo');
+
+        $this->assertInternalType('array', $torrents);
+        $this->assertCount(25, $torrents);
+        $this->assertInstanceOf('Axon\Search\Model\Torrent', $torrents[0]);
+        $this->assertEquals('Iron Man 3 (2013) 1080p BrRip x264 - YIFY', $torrents[0]->getName());
+        $this->assertEquals('70B487B9E21E2869AF831397851F45A00D3EA7CA', $torrents[0]->getHash());
+        $this->assertEquals(1950000000, $torrents[0]->getSize());
+        $this->assertEquals(3038, $torrents[0]->getSeeds());
+        $this->assertEquals(321, $torrents[0]->getPeers());
+    }
+
+    /**
+     * @test
+     * @expectedException Axon\Search\Exception\ConnectionException
+     */
+    public function shouldThrowExceptionOnConnectionError()
+    {
+        $browser = $this->getMock('Buzz\Browser');
+        $browser
+            ->expects($this->once())
+            ->method('get')
+            ->will($this->throwException(new \Exception()));
+
+        $provider = new KickassProvider($browser);
+        $provider->search('foo');
+    }
+
+    /**
+     * @test
+     * @expectedException Axon\Search\Exception\UnexpectedResponseException
+     */
+    public function shouldThrowExceptionOnUnexpectedResponse()
+    {
+        $response = $this->getMock('Buzz\Message\Response');
+        $response
+            ->expects($this->any())
+            ->method('getStatusCode')
+            ->will($this->returnValue(400));
+
+        $response
+            ->expects($this->any())
+            ->method('getReasonPhrase')
+            ->will($this->returnValue('foo'));
+
+        $browser = $this->getMock('Buzz\Browser');
+        $browser
+            ->expects($this->any())
+            ->method('get')
+            ->will($this->returnValue($response));
+
+        $provider = new KickassProvider($browser);
+        $provider->search('foo');
+    }
+
+    /**
+     * @test
+     * @dataProvider urlProvider
+     */
+    public function shouldGenerateCorrectUrl($query, $page, $expected)
+    {
+        $provider = new KickassProvider();
+
+        $this->assertEquals($expected, $provider->getUrl($query, $page));
+    }
+
+    public function urlProvider()
+    {
+        return array(
+            array('foo', null, 'http://kickass.to/usearch/foo/?field=seeders&order=desc'),
+            array('foo', 1, 'http://kickass.to/usearch/foo/1/?field=seeders&order=desc'),
+        );
+    }
+}
